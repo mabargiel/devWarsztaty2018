dc4f848f551e1b207de100ca4e55874d0dfadd97
Mateusz Bargiel <mateusz@theministryofideas.com>
2018-01-27 11:31:51 +0100

    SaveAnOrder func

---
+++ OrderProcessing/SaveAnOrder.cs
@@ -0,0 +1,37 @@
+
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace OrderProcessing
+{
+    public static class SaveAnOrder
+    {
+        [FunctionName("SaveAnOrder")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            try
+            {
+                var requestBody = new StreamReader(req.Body).ReadToEnd();
+                var order = JsonConvert.DeserializeObject<Order>(requestBody);
+            }
+            catch (Exception)
+            {
+                return new BadRequestObjectResult("Received data invalid");
+            }
+        }
+    }
+
+    public class Order
+    {
+        public string Email { get; set; }
+        public string FileName { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+}
